b98a9a7b54d29a72c0be0168dbad58fbe28ff607
CorentinDupont <dupont.corentin44@gmail.com>
2020-02-13 13:31:31 +0100

    ✅ test(author, post) : add get one post get one author test

--- spec/api.test.js
+++ spec/api.test.js
@@ -19,6 +19,34 @@ describe('GET /', () => {
     expect(response.body.data.posts[0].author.name).toBe("Xavier Decuyper")
     console.log(response.body)
     })
+
+    test('verify send one post has author', async () =>{
+      const query_string = `{
+        post(id: 1) {
+          title
+          author {
+            name
+          }
+        }
+      }`
+      response  = await  request(app)
+        .post('/graphql').send({ query:  query_string })
+      expect(response.body.data.post.title).toBe("My first blog post")
+      expect(response.body.data.post.author.name).toBe("Xavier Decuyper")
+      console.log(response.body)
+    })
+
+    test('verify send one author has name', async () =>{
+      const query_string = `{
+        author(id: "88d6bec2") {
+          name
+        }
+      }`
+      response  = await  request(app)
+        .post('/graphql').send({ query:  query_string })
+      expect(response.body.data.author.name).toBe("Xavier Decuyper")
+      console.log(response.body)
+    })
 });
 
 // describe('POST/', () => {
