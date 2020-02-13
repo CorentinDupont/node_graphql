3315d8e60c88dbbf71799a5e4bd1b6949d806728
CorentinDupont <dupont.corentin44@gmail.com>
2020-02-13 11:12:36 +0100

    ✅ test(posts) : update get posts test with author name

--- spec/api.test.js
+++ spec/api.test.js
@@ -6,13 +6,17 @@ describe('GET /', () => {
 
   test('verify send many posts has authors', async () =>{
     const query_string = `{
-      posts{
+      posts {
         title
+        author {
+          name
+        }
       }
     }`
     response  = await  request(app)
       .post('/graphql').send({ query:  query_string})
     expect(response.body.data.posts[0].title).toBe("My first blog post")
+    expect(response.body.data.posts[0].author.name).toBe("Xavier Decuyper")
     console.log(response.body)
     })
 });
